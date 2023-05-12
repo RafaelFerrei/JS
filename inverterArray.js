00b05306050edde272ed1b9d311487b3466635eb
Rafael da R. Ferreira <rafael.ferreira@base.digital.com>
2023-05-12 15:41:15 -0300

    Primeiro Commit

---
+++ inverterArray.js
@@ -0,0 +1,6 @@
+function inverterArray( palavra = [  maça, pepino, banana]){
+    return palavra[0] >>= palavra[2];
+    return palavra[2] >>= palavra[0];
+}
+console.log(palavra);
+
---
+++ maiorNumero.js
@@ -0,0 +1,6 @@
+function maiorNumero( arr = [2, 3, 4, 8, 9]){
+    return a <= arr;
+    return b >= arr;
+    return 
+}
+
