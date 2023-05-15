1388a4111556cd49d1277e8eae4bccd3c535899b
Rafael da R. Ferreira <rafael.ferreira@base.digital.com>
2023-05-15 15:05:01 -0300

    Opa

--- inverterArray.js
+++
@@ -1,6 +0,0 @@
-function inverterArray( palavra = [  maça, pepino, banana]){
-    return palavra[0] >>= palavra[2];
-    return palavra[2] >>= palavra[0];
-}
-console.log(palavra);
-
--- maiorNumero.js
+++
@@ -1,6 +0,0 @@
-function maiorNumero( arr = [2, 3, 4, 8, 9]){
-    return a <= arr;
-    return b >= arr;
-    return 
-}
-
